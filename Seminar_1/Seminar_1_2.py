fd8124c6c87cd7319aaa262c2825cc3091eb8311
Maxim Grekov <grecheskiy@rambler.ru>
2023-11-16 00:00:01 +0300

    add Seminar_1

--- Seminar_1/Seminar_1_2.py
+++ Seminar_1/Seminar_1_2.py
@@ -5,8 +5,8 @@ TEN = 10
 HUNDRED = 100
 SQUARE = 2
 
-num = LOWER_LIMIT
-while num < LOWER_LIMIT or UPPER_LIMIT:
+num = LOWER_LIMIT - ONE
+while num < LOWER_LIMIT or num > UPPER_LIMIT:
     num = int(input(f'Enter number from {LOWER_LIMIT} till {UPPER_LIMIT}'))
 if num < TEN:
     res = f'Number {num} number. It square = {num**SQUARE}'
