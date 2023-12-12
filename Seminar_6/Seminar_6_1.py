882f4d496835882126097af930595fdf7c2def26
Maxim Grekov <grecheskiy@rambler.ru>
2023-12-12 23:57:42 +0300

    add Seminar_6

---
+++ Seminar_6/Seminar_6_1.py
@@ -0,0 +1,8 @@
+# Вспомните какие модули вы уже проходили на курсе
+# Создайте файл, в котором вы импортируйте встроенные в модуль функции под псевдонимами
+
+from random import randint as rnd
+from decimal import Decimal as D
+from math import sqrt as sq
+from fractions import Fraction as F
+from sys import argv as ar
